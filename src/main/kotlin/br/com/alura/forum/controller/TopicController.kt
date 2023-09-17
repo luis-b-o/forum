09e82450db2a3fe791dba4db226cff483ef1ac13
Luis Oliveira <luis.oliveira@startse.com>
2023-09-17 07:33:31 -0300

    feat: add bean validation on register topic

--- src/main/kotlin/br/com/alura/forum/controller/TopicController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicController.kt
@@ -3,6 +3,7 @@ package br.com.alura.forum.controller
 import br.com.alura.forum.dto.NewTopicForm
 import br.com.alura.forum.dto.TopicView
 import br.com.alura.forum.service.TopicService
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -20,7 +21,7 @@ class TopicController(private val service: TopicService) {
     }
 
     @PostMapping
-    fun register(@RequestBody topic: NewTopicForm) {
+    fun register(@RequestBody @Valid topic: NewTopicForm) {
         service.register(topic)
     }
 
